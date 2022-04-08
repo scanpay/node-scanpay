e5d41dfdad22d1c4f8568a0fe93123d8e19681eb
Ulrik Moe <ulrik.moe@gmail.com>
2022-04-08 21:15:14 +0200

    Add scanpay.ip()

--- tests/sync.parsePing.js
+++ tests/sync.parsePing.js
@@ -4,33 +4,45 @@
     help@scanpay.dk || irc.libera.chat:6697 #scanpay
 */
 
-const apikey = '1153:YHZIUGQw6NkCIYa3mG6CWcgShnl13xuI7ODFUYuMy0j790Q6ThwBEjxfWFXwJZ0W';
+const apikey = '6145:NuzXfh1NlbOS/ix5L9gExfPlc8KOQXZ9i43e30HSFoIspKPFaeb2Is4WzCqfCA5Q';
 const scanpay = require('../')(apikey);
 const http = require('http');
-
-const options = {
-    debug: true
-};
 
 const server = http.createServer((req, res) => {
     let body = '';
     req.on('data', chunk => (body += chunk));
     req.on('end', () => {
-        try {
-            const json = scanpay.handlePing(
-                body,
-                req.headers['x-signature'],
-                options
-            );
-            console.log('Ping:\n' + JSON.stringify(json, null, 4) + '\n');
-        } catch (e) {
-            console.log(e);
-        }
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
+        res.writeHead(200);
         res.end();
+
+        // We want to close the connection ASAP and parse the ping
+        // asynchronously to limit exposure to side-channel attacks.
+        scanpay.sync.parsePing(body, req.headers['x-signature'])
+            .then(json => console.log(json))
+            .catch((err) => {
+                if (err.type !== 'ScanpaySignatureError') console.log(err);
+                else console.log(err); // Invalid pings
+            });
     });
 });
 
+server.on('connection', (sock) => {
+    // Only allow scanpay IPs (optional)
+    if (!scanpay.ip(sock.remoteAddress)) {
+        console.log('invalid ping IP!!');
+        sock.destroy();
+    }
+});
+
+// Protect against DoS attacks
+server.headersTimeout = 2000;
+server.requestTimeout = 3000;
+server.maxHeaderSize = 2000;
+
+// We batch pings, but you can still receive
+// up to 100 pings per minute, so keep-alive
+// is a good idea.
+server.keepAliveTimeout = 60000;
+
 server.listen(8080);
+
